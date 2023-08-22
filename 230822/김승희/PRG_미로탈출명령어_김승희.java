c1601d8762666195802c398c2f26934182082457
seunghee <43427305+seunghee114@users.noreply.github.com>
2023-08-22 21:24:12 +0900

    230822 김승희 제출

--- 230822/김승희/PRG_미로탈출명령어_김승희.java
+++ 230822/김승희/PRG_미로탈출명령어_김승희.java
@@ -1,53 +1,71 @@
-def answer(sub, origin, N):
-    ans = []
-    for i in range(N-origin[0]):
-        ans.append('a')
-    return ''.join(ans) + sub
-def process(origin, K, N):
-    ans = []
-    index = [origin[0], origin[1]]
-    while True:
-        # a : dp[index[0]-1][index[1]] 경우의 수, 
-        # 즉, a를 index[0]-1개, z를 index[1]개 썼을 때 앞에 a를 붙이는 경우의 수 
-        a = dp[index[0]-1][index[1]]    
-        # z : dp[index[0]][index[1]-1] 경우의 수, 
-        # 즉, a를 index[0]개, z를 index[1]-1개 썼을 때 앞에 z를 붙이는 경우의 수 
-        z = dp[index[0]][index[1]-1]
-        if K > a:   # K가 a보다 크면 z 경우에 해당
-            K -= a
-            ans.append('z')
-            if index[1]-1 == 0:
-                for i in range(index[0]):
-                    ans.append('a')
-                break
-            index = [index[0], index[1]-1]
-        else:
-            ans.append('a')
-            if index[0]-1 == 0:
-                for i in range(index[1]):
-                    ans.append('z')
-                break
-            index = [index[0]-1, index[1]]
-    return answer(''.join(ans), origin, N)
-
-# N : a의 개수
-# M : z의 개수
-# K : k번째 문자열
-N, M, K = map(int, input().split(" "))
-# dp[i][j] = a를 i개, z를 j개 썼을 때 경우의 수
-dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
-index = [N, M]
-for i in range(1, N+1):
-    # a만 있는 경우는 1개
-    dp[i][0] = 1
-for i in range(1, M+1):
-    # z만 있는 경우는 1개
-    dp[0][i] = 1
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        # dp[i][j] = a를 i-1개 z를 j개 쓴 문자열 앞에 a 붙이기 + a를 i개 z를 j-1개 쓴 문자열 앞에 z 붙이기
-        dp[i][j] = dp[i-1][j] + dp[i][j-1]
-    if dp[i][M] >= K:
-        index[0] = i
-        break
-print(-1 if dp[index[0]][index[1]] < K  else process(index, K, N))
+import java.util.*;
+class Solution {
+    static int N, M, K;
+    static Tuple Start, End;
+    static int[] di = {1, 0, 0, -1};
+    static int[] dj = {0, -1, 1, 0};
+    static class Tuple{
+        int x, y;
+        public Tuple(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        int d;
+        public Tuple(int x, int y, int d){
+            this.x = x;
+            this.y = y;
+            this.d = d;
+        }
+        
+    }
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        String answer = "";
+        N = n; 
+        M = m;
+        K = k;
+        Start = new Tuple(x-1, y-1);
+        End = new Tuple(r-1, c-1);
+        
+        return BFS();
+    }
+    static String BFS(){
+        boolean[][][] visit = new boolean[N][M][K+1];
+        Tuple[][][] prev = new Tuple[N][M][K+1];
+        Queue<Tuple> Q = new ArrayDeque<>();
+        visit[Start.x][Start.y][0] = true;
+        Q.add(new Tuple(Start.x, Start.y));
+        
+        int dist = 0;
+        while(!Q.isEmpty()){
+            int size = Q.size();
+            for(int p = 0; p < size; p++){
+                Tuple tp = Q.poll();
+                for(int k = 0; k < 4; k++){
+                    int ni = tp.x + di[k];
+                    int nj = tp.y + dj[k];
+                    if (ni < 0 || nj < 0 || ni >= N || nj >= M || visit[ni][nj][dist+1]) continue;
+                    visit[ni][nj][dist+1] = true;
+                    prev[ni][nj][dist+1] = new Tuple(tp.x, tp.y, k);
+                    if(dist+1 == K && ni == End.x && nj == End.y){
+                        return answer(prev);
+                    }
+                    Q.add(new Tuple(ni, nj));
+                }
+            }
+            dist++;
+            if(dist == K) break;
+        }
+        return "impossible";
+    }
+    static char[] dir = {'d', 'l', 'r', 'u'};
+    static String answer(Tuple[][][] prev){
+        StringBuffer sb = new StringBuffer();
+        int dist = K;
+        Tuple tp = prev[End.x][End.y][dist--];
+        while(tp != null){
+            sb.append(dir[tp.d]);
+            tp = prev[tp.x][tp.y][dist--];
+        }
+        return sb.reverse().toString();
+    }
+}
