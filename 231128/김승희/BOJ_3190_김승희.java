9c039bb5fc79c76e7f604a63ee67d63df7c3bc04
seunghee <43427305+seunghee114@users.noreply.github.com>
2023-11-28 21:24:24 +0900

    Update BOJ_3190_김승희.java

--- 231128/김승희/BOJ_3190_김승희.java
+++ 231128/김승희/BOJ_3190_김승희.java
@@ -1,77 +1,70 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-    static int N, D, Time;
-    static int[] di = {0, 1, 0, -1};
-    static int[] dj = {1, 0, -1, 0};
-    static boolean[][] apple, isSnake;
-    static Deque<int[]> Q;
-    static HashMap<Integer, String> direction;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        apple = new boolean[N][N];      // apple[i][j] : (i, j) 좌표에 사과가 있으면 true
-        isSnake = new boolean[N][N];    // isSnake[i][j] : (i, j) 좌표가 뱀이면 true
-        int M = Integer.parseInt(br.readLine());    // M : 사과의 개수
-        for (int i = 0; i < M; i++) {
-            String[] inArr = br.readLine().split(" ");
-            int a = Integer.parseInt(inArr[0])-1;
-            int b = Integer.parseInt(inArr[1])-1;
-            apple[a][b] = true;
-        }
-        direction = new HashMap<>();    // key : time, value : 방향.
-        M = Integer.parseInt(br.readLine());    // M : 방향 변환 횟수
-        for (int i = 0; i < M; i++) {
-            String[] inArr = br.readLine().split(" ");
-            int a = Integer.parseInt(inArr[0]);
-            direction.put(a, inArr[1]);
-        }
-        // end input
-
-        Q = new ArrayDeque<>();     // Q의 맨 앞 : 뱀의 꼬리, Q의 맨 뒤 : 뱀의 머리
-        Q.add(new int[]{0, 0});
-        isSnake[0][0] = true;
-        D = 0;      // D : 뱀의 이동 방향. 처음엔 오른쪽으로
-        Time = 0;   // Time : 절대 시간
-        while (true) {
-            // 뱀 움직이기
-            if (!move()) break;
-            Time++;
-            String nd = direction.getOrDefault(Time, "NO");
-            if (nd.equals("NO")) continue;
-            if (nd.equals("D")) {
-                D = (D + 1) % 4;
-            } else if (nd.equals("L")) {
-                D = (D - 1 + 4) % 4;
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> tallerMan = new ArrayList<>();       // 자기보다 키가 큰 여자랑 춤추고 싶은 남자
+        ArrayList<Integer> shorterMan = new ArrayList<>();      // 자기보다 키가 작은 여자랑 춤추고 싶은 남자
+        String[] inArr = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            int height = Integer.parseInt(inArr[i]);
+            if (height > 0) {
+                tallerMan.add(height);
+            } else {
+                shorterMan.add(-height);
             }
         }
-        System.out.println(Time + 1);
-    }
-    static boolean move() {
-        if (Q.isEmpty()) return false;
-        int[] now = Q.peekLast();
-        // 새로운 머리 위치
-        int headI = now[0] + di[D];
-        int headJ = now[1] + dj[D];
-        // 벽을 만나면 게임 끝
-        if (headI >= N || headJ >= N || headI < 0 || headJ < 0) return false;
-        // 자기 몸을 만나면 끝
-        if (isSnake[headI][headJ]) return false;
-        if (apple[headI][headJ]) {  // 사과가 있으면 사과 먹기
-            apple[headI][headJ] = false;
-        }else {     // 사과가 없으면 꼬리 말기
-            int[] tail = Q.poll();
-            isSnake[tail[0]][tail[1]] = false;
+
+        ArrayList<Integer> tallerWoman = new ArrayList<>();     // 자기보다 키가 큰 남자랑 춤추고 싶은 여자
+        ArrayList<Integer> shorterWoman = new ArrayList<>();    // 자기보다 키가 작은 남자랑 춤추고 싶은 여자
+        inArr = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            int height = Integer.parseInt(inArr[i]);
+            if (height > 0) {
+                tallerWoman.add(height);
+            } else {
+                shorterWoman.add(-height);
+            }
         }
-        Q.add(new int[]{headI, headJ});
-        isSnake[headI][headJ] = true;
-        return true;
+
+        // 오름차순 정렬
+        Collections.sort(tallerMan);
+        Collections.sort(tallerWoman);
+        Collections.sort(shorterMan);
+        Collections.sort(shorterWoman);
+
+        int answer  = 0;   // answer : 만들 수 있는 커플의 수
+
+        // 남자 1번 유형 + 여자 2번 유형
+        answer += match(tallerMan, shorterWoman);
+        // 남자 2번 유형 + 여자 1번 유형
+        answer += match(tallerWoman, shorterMan);
+
+        System.out.println(answer);
+    }
+
+    // taller : 키가 큰 애를 만나고 싶음
+    // shorter : 키가 작은 애를 만나고 싶음
+    static int match(ArrayList<Integer> taller, ArrayList<Integer> shorter) {
+        int acc = 0;
+        int t = 0;
+        int s = 0;
+
+        while (t < taller.size() && s < shorter.size()) {
+            if (taller.get(t) < shorter.get(s)) {   // shorter가 taller보다 키가 크면 -> 커플 만들기
+                acc++;
+                t++;
+                s++;
+            } else {    // shorter가 taller보다 키가 작거나 같으니까 -> 커플 못 만듬
+                // shorter가 더 커야 되니까 다음 친구로
+                s++;
+            }
+        }
+        return acc;
     }
 }
